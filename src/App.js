3ddbae23f9d0ba6b5df4568b86eabc3b08e38c00
Jaan Paljasma <jaan@hebsdigital.com>
2020-03-01 08:31:20 -0500

    Updated App, added 404 handler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 const HatsPage = () => {
@@ -10,11 +10,24 @@ const HatsPage = () => {
   )
 };
 
+const FourOhFour = ({ location, match }) => {
+  return (
+    <div className='error'>
+      <h1>404</h1>
+      <p>The requested document <code>{location.pathname}</code> was not found.</p>
+      <p>Please proceed to <Link to={match.url}>home page</Link>.</p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route component={FourOhFour} status={404}/>
+      </Switch>
     </div>
   );
 }
