450d851c2fa0fe065fa37f3d50605c76744eeda0
Jaan Paljasma <jaan@hebsdigital.com>
2020-03-01 21:06:43 -0500

    Added a sign in form with custom form element and button components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import NotFound from './pages/notfound/notfound.component';
 import Header from './components/header/header.component';
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
         <Route component={NotFound} status={404} />
       </Switch>
     </div>
---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>Sign Up</div>
+    )
+  }
+}
+export default SignUp;
